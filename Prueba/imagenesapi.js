87384c1cf2e01906844b91bb6b8c30c2ce7ec630
Jsmorales23 <90493185+Jsmorales23@users.noreply.github.com>
2021-09-10 23:58:34 -0500

    Add files via upload

---
+++ Prueba/imagenesapi.js
@@ -0,0 +1,24 @@
+/* let url = "https://api.unsplash.com/search/photos?query=all&client_id=PYY3q6HNb60EehBacxC8TNW0A8orDHMTV1kTh0AjraI";
+let requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+let imagenesDiv = document.getElementById("imagenesjson");
+fetch(url, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    let fotos = result.results;
+    console.log(fotos)
+    for (let foto of fotos) {
+      let nuevoDiv = document.createElement("div");
+      let imgApi = foto.urls.small;
+      let markHtml = `
+        <div class="box">
+            <img id="" src="${imgApi}" alt="">
+        </div>
+      `;
+      nuevoDiv.innerHTML = markHtml;
+      imagenesDiv.appendChild(nuevoDiv);
+    }
+  })
+  .catch(error => console.log('error', error)); */
